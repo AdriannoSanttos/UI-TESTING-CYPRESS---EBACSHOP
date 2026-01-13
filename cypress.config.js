eb2779a4c26b506690edca418bc1b013809b1cbd
Adriano Santtos <j.adriannosanttos@gmail.com>
2026-01-13 17:03:15 -0300

    feat: adiciona pipeline CI com Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
